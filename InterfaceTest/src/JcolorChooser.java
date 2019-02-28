866a8eee0d7b2ba0ddb5571df37c08896c710bcc
stiff <stiff@DESKTOP-R5O62L6>
2019-02-28 18:02:25 +0100

    test

--- InterfaceTest/src/JcolorChooser.java
+++ InterfaceTest/src/JcolorChooser.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 public class JcolorChooser {
 	public JcolorChooser() {
 		JFrame hauptFenster = new JFrame("JColor");
-		
+		hauptFenster.setSize(200, 200);
 	}
 
 	public static void main(String args[]) {
