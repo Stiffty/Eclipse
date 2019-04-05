29dee05518c72f2246c8905a23d44f92adcf12a3
achtniel <achtniel@r403-c07>
2019-04-05 11:46:02 +0100

    Sortirungs algorytmus funktonirt noch nicht

--- Schiffeversenken/src/Sortirung.java
+++ Schiffeversenken/src/Sortirung.java
@@ -1,10 +1,11 @@
-import java.util.*;
+import java.util.Random;
 
-public class Sotieren {
+public class Sortirung {
 
-	public Sotieren() {
+	public Sortirung() {
 		// TODO Auto-generated constructor stub
-		int[] Feld = new int[1000000];
+		int[] Feld = new int[100];
+
 		int test = 0;
 		int test1 = 0;
 
@@ -13,7 +14,7 @@ public class Sotieren {
 		System.out.println("Davor");
 		for (int i = 0; i < Feld.length; i++) {
 			System.out.print(Feld[i] + " ");
-			test++; 
+			test++;
 		}
 		System.out.println("");
 
@@ -26,29 +27,55 @@ public class Sotieren {
 		}
 		System.out.println("");
 		System.out.println(test + " " + test1);
-		
+
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		new Sotieren();
+		new Sortirung();
 	}
 
 	public int[] füllung(int[] Feld) {
+		int[] Test = new int[100];
+		int test = 0;
 		Random rand = new Random();
-		for (int i = 0; i < Feld.length; i++) {
-			Feld[i] = rand.nextInt(50);
+
+		for (int i = Feld.length / 2; i < Feld.length; i++) {
+			test = 0;
+			while (test == 0) {
+				Feld[i] = rand.nextInt(50);
+				Test[Feld[i]]++;
+				test = 1;
+				if (Test[Feld[i]] > 1) {
+					test = 0;
+				}
+			}
 		}
 		return Feld;
 
 	}
 
 	public int[] sortieren(int[] Feld) {
+		int max = 1;
 		int a;
 		int b;
-		for (int j = 0; j < Feld.length; j++) {
+		int c = 0;
+		
+		int test = 0;
 
-			for (int i = 0; i < Feld.length; i++) {
+		for (int i = (Feld.length / 2)-1; i < Feld.length; i++) {
+
+			while (test == 0) {
+				if (Feld[i] == max) {
+					Feld[c] = Feld[i];
+					c++;
+					max++;
+					test = 1;
+				} else {
+					test = 0;
+					
+				}
+				
 				if (i == 0) {
 					if (Feld[i] > Feld[i + 1]) {
 						a = Feld[i];
@@ -82,12 +109,12 @@ public class Sotieren {
 						Feld[i] = b;
 						Feld[i - 1] = a;
 					}
+					
+					
 				}
 			}
-			
 		}
 		return Feld;
 	}
-
 
 }
