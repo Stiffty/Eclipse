b7f689bf62a0dd552ca7d42cad34f94eff741c7f
achtniel <achtniel@r403-c07>
2019-04-05 09:02:18 +0100

    Sotieren

---
+++ Schiffeversenken/src/Sotieren.java
@@ -0,0 +1,13 @@
+
+public class Sotieren {
+
+	public Sotieren() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
