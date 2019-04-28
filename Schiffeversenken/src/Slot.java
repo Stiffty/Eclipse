d7ab061640d23ed9c8b5100c08074e991277e637
elach <elach@DESKTOP-0BLGD50>
2019-04-28 22:22:28 +0200

    Slot

--- Schiffeversenken/src/Slot.java
+++ Schiffeversenken/src/Slot.java
@@ -5,6 +5,8 @@ public class Slot {
 	public Slot() {
 		// TODO Auto-generated constructor stub
 		int zahl;
+		int ziehen = 100;
+		int leute = 5000000;
 		int konto = 400;
 		int gewinn = 0;
 		int allesweg = 0; 
@@ -18,11 +20,11 @@ public class Slot {
 		int g5 = 0;
 		int g1 = 0;
 		Random rand = new Random();
-		for (int j = 0; j < 50000; j++) {
+		for (int j = 0; j < leute; j++) {
 			
 		
 		konto = 50;
-		for (int i = 0; i <1000; i++) {
+		for (int i = 0; i <ziehen; i++) {
 			
 		konto-=2;
 		gewinn+=2;
@@ -31,44 +33,44 @@ public class Slot {
 		ziehungen++;
 		
 		if (zahl == 1 ) {
-			System.out.println("Hauptgewinn");
+			//System.out.println("Hauptgewinn");
 			konto+=500;
 			gewinn-=500;
 			hauptgewinn++;
 		}else if (zahl == 2||zahl == 3) {
-			System.out.println("100€");
+			//System.out.println("100€");
 			konto+=100;
 			gewinn-=100;
 			g100++;
 		}else if (zahl >= 4&&zahl <= 7) {
-			System.out.println("50€");
+			//System.out.println("50€");
 			konto+=50;
 			gewinn-=50;
 			g50++;
 		}else if (zahl >= 8&&zahl <= 15) {
-			System.out.println("10€");
+			//System.out.println("10€");
 			konto+=10;
 			gewinn-=10;
 			g10++;
-		}else if (zahl >= 16&&zahl <= 31) {
-			System.out.println("5€");
+		}else if (zahl >= 16&&zahl <= 30) {
+			//System.out.println("5€");
 			konto+=5;
 			gewinn-=5;
 			g5++;
 		}else if (zahl>183&&zahl<367) {
-			System.out.println("1€");
+			//System.out.println("1€");
 			konto+=1;
 			gewinn-=1;
 			g1++;
 		}else {
-			System.out.println("Niete");
+			//System.out.println("Niete");
 		}
 		if (konto <= 0) {
-			i = 1000;
+			i = ziehen;
 			allesweg++;
 		}
 		}
-		if (konto>100) {
+		if (konto>50) {
 			gewinner++;
 		}
 		if (konto>höchstgewinn) {
@@ -76,13 +78,13 @@ public class Slot {
 		}
 		System.out.println("Konto: " + konto + "€");
 		}
-		System.out.println("500€ " + hauptgewinn + "\n100€ " + g100+ "\n50€ " + g50
-				+ "\n10€ " + g10+ "\n5€ " + g5 + "\n1€ "+g1 + "\n"+
+		System.out.println("500€ " + hauptgewinn + "  "+(double)  (hauptgewinn*100)/ziehungen + "%" + "\n100€ " + g100+  "  "+(double)  (g100*100)/ziehungen + "%" +"\n50€ " + g50
+				+  "  "+(double)  (g50*100)/ziehungen + "%" +"\n10€ " + g10+ "  "+ (double) (g10*100)/ziehungen + "%" + "\n5€ " + g5 +  "  "+(double)  (g5*100)/ziehungen + "%" + "\n1€ "+g1 +  "  "+((double )g1*100)/ziehungen + "%" +"\n"+
 				"Ziehungen: " + ziehungen + "\n" +
 				"HöchstGewinn: " + höchstgewinn + "\n" +
-				"Gewinner: " + gewinner + "\n"+
-				"alles WEG: " + allesweg + "\n"+
-				"Gewinn: " + gewinn + "€");
+				"Gewinner: " + gewinner + "  "+ (gewinner*100)/leute + "%" +"\n"+
+				"alles WEG: " + allesweg +  "  "+ (allesweg*100)/leute + "%" +"\n"+
+				"Gewinn: " + gewinn + "€" + "\n" + "Durschnitt: " +(double )gewinner/leute);
 	}
 
 	public static void main(String[] args) {
