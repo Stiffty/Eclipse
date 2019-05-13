a1fc6b80df7b1eb206a5852db80e300fc64b85e9
achtniel <achtniel@r124-c09>
2019-05-13 08:24:38 +0100

    dual rechner

---
+++ Schiffeversenken/src/Schule/DualRechnen.java
@@ -0,0 +1,41 @@
+package Schule;
+
+import java.util.Scanner;
+
+public class DualRechnen {
+
+	private int eingabea;
+	private int eingabeb;
+	
+	public DualRechnen() {
+		// TODO Auto-generated constructor stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("A");
+		eingabea=sc.nextInt();
+		
+		System.out.println("b");
+		eingabeb = sc.nextInt();
+		
+		BinZuDual();
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new DualRechnen();
+	}
+	
+	public void BinZuDual(){
+		int zahl = 0;
+		for (int i = 0;i<=151 ;i++ ) {
+		      zahl = zahl%23;
+		       
+		      if (zahl == 0) {
+		        System.out.println(i);
+		      }
+		      zahl = i+1;
+		    }
+
+	}
+
+}
