340d9b830ef4edb5c6f26709a86d2abf7f6ceb76
elach <elach@DESKTOP-0BLGD50>
2019-06-02 13:20:50 +0200

    Caesar

--- Schiffeversenken/src/Schule/Caesar.java
+++ Schiffeversenken/src/Schule/Caesar.java
@@ -1,5 +1,6 @@
 package Schule;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class Caesar {
@@ -7,22 +8,34 @@ public class Caesar {
 	public Caesar() {
 		// TODO Auto-generated constructor stub
 		Scanner sc = new Scanner(System.in);
-		String eingabe = sc.nextLine();
+		
+		System.out.println("Verschlüsselung(1) Entschlüsselung(0)");
+		int verschlüsselung = sc.nextInt();
+		
+		System.out.println("Wort");
+		String eingabe = sc.next();
 		char[] eingabech = eingabe.toCharArray();
+		
+		byte[] bytes = eingabe.getBytes(StandardCharsets.US_ASCII);
+		
+		System.out.println("zahl");
 		int verschiebung = sc.nextInt();
-		byte[] bytes = eingabe.getBytes("US-ASCII");
+		
 		for (int i = 0; i < eingabech.length; i++) {
-			bytes[i]+=1;	
+			if(verschlüsselung == 1) {
+			bytes[i]+=verschiebung;	
+			}else if(verschlüsselung == 0) {
+				bytes[i]-=verschiebung;	
+			}
 			}
 		
-		
-	    
+		    System.out.println(new String(bytes, StandardCharsets.US_ASCII));
 		
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		new Caesar();
 	}
 
 }
