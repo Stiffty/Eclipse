8d65262548f07874505ec1aede47672799cda558
elach <elach@DESKTOP-0BLGD50>
2019-11-18 19:45:25 +0100

    dsa

--- Schiffeversenken/src/FrimenSoftware/FirmaGUI.java
+++ Schiffeversenken/src/FrimenSoftware/FirmaGUI.java
@@ -6,6 +6,12 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+import java.awt.Color;
+import javax.swing.JSeparator;
+import javax.swing.JSplitPane;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 
 public class FirmaGUI extends JFrame {
 
@@ -33,11 +39,36 @@ public class FirmaGUI extends JFrame {
 	 */
 	public FirmaGUI() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 560, 341);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblTest = new JLabel("test");
+		lblTest.setBounds(103, 34, 46, 14);
+		contentPane.add(lblTest);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.GREEN);
+		panel.setToolTipText("test");
+		panel.setForeground(Color.WHITE);
+		panel.setBorder(new LineBorder(Color.RED, 3));
+		panel.setBounds(5, 196, 424, 60);
+		contentPane.add(panel);
+		
+		JSplitPane splitPane = new JSplitPane();
+		splitPane.setBounds(41, 88, 268, 46);
+		contentPane.add(splitPane);
+		
+		JSeparator separator = new JSeparator();
+		splitPane.setLeftComponent(separator);
+		
+		JButton btnNewButton = new JButton("New button");
+		splitPane.setRightComponent(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("New button");
+		btnNewButton_1.setBounds(335, 149, 89, 23);
+		contentPane.add(btnNewButton_1);
 	}
-
 }
--- Schiffeversenken/src/Schule/RekusionPalindrom.java
+++ Schiffeversenken/src/Schule/RekusionPalindrom.java
@@ -1,11 +1,39 @@
 package Schule;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.Scanner;
+import java.util.jar.JarException;
+
+import javax.tools.JavaFileObject;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 public class RekusionPalindrom {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		JsonReader jr = new JsonReader();
+		JSONObject json;
+		try {
+			json = jr.readJsonFromUrl("https://cc.the-morpheus.de/challenges/15/");
+			System.out.println(json.toString());
+		    System.out.println(json.get("id"));
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    
 
 		
 		Scanner sc = new Scanner(System.in);
@@ -33,4 +61,31 @@ public class RekusionPalindrom {
 			}
 		}
 	}
+	
+	 
 }
+class JsonReader {
+
+	  private static String readAll(Reader rd) throws IOException {
+	    StringBuilder sb = new StringBuilder();
+	    int cp;
+	    while ((cp = rd.read()) != -1) {
+	      sb.append((char) cp);
+	    }
+	    return sb.toString();
+	  }
+
+	  public JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
+	    InputStream is = new URL(url).openStream();
+	    try {
+	      BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+	      String jsonText = readAll(rd);
+	      JSONObject json = new JSONObject(jsonText);
+	      return json;
+	    } finally {
+	      is.close();
+	    }
+	  }
+
+	  
+	}
