9e0cd8d003d3d5f5a7e06139fb878fbd3df87233
achtniel <achtniel@r306-c02>
2019-12-11 10:43:54 +0100

    hafen sys

---
+++ Schiffeversenken/src/hafenSystem/FSchiff.java
@@ -0,0 +1,13 @@
+package hafenSystem;
+
+public class FSchiff extends Schiff {
+	
+	
+	private int ladung;
+	
+	public FSchiff(String id,String n,int i) {
+		// TODO Auto-generated constructor stub
+		super(id, n, i);
+	}
+
+}
---
+++ Schiffeversenken/src/hafenSystem/Seestraße.java
@@ -0,0 +1,19 @@
+package hafenSystem;
+
+public class Seestraﬂe {
+
+	private String bezeichnung;
+	private Schiff schiff;
+	public Seestraﬂe(String b) {
+		// TODO Auto-generated constructor stub
+		
+	}
+	
+	public void belegen(Schiff s){
+		
+	}
+	public boolean istFrei(){
+		
+	}
+
+}
