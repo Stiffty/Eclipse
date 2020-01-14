7670736a5958a40b711fd23e16e04eff2bc1c16a
elach <el.ach@gmx.de>
2020-01-14 20:33:11 +0100

    First Api tests failed

--- APIs/src/firstTest/FirstTest.java
+++ APIs/src/firstTest/FirstTest.java
@@ -1,10 +1,56 @@
 package firstTest;
 
+import sun.net.www.http.HttpClient;
+import java.net.*;
+
 public class FirstTest {
 
+	//private static HttpURLConnection connection
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+/*		Methode 1:
+        ----------
+        
+		BufferedReader reader;
+		String line;
+		StringBuffer responseContent = new StringBuffer();
 
-	}
+		try {
+			URL url = new URL("https://oauth.reddit.com/");
+			connection = (HttpURLConnection) url.openConnection();
 
+			// Request setup
+			connection.setRequestMethod("GET");
+			connection.setConnectTimeout(5000);
+			connection.setReadTimeout(5000);
+
+			int status = connection.getResponseCode();
+//			System.out.println(status);
+			// Input Stream auslesen
+
+			if (status > 299) {
+				reader = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+				while ((line = reader.readLine()) != null) {
+					responseContent.append(line);
+				}
+				reader.close();
+			} else {
+				reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+				while ((line = reader.readLine()) != null) {
+					responseContent.append(line);
+				}
+				reader.close();
+			}
+
+			System.out.println(responseContent.toString());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			connection.disconnect();
+		}
+*/
+		//Methode 2:
+		//__________
+		
 }
